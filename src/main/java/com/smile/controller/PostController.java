153c678a170880950663d89db965a7596b62c863
Gyunny <wjdrbs966@naver.com>
2021-10-30 11:27:17 +0900

    Feat: JSP 세팅 완료

--- src/main/java/com/smile/controller/PostController.java
+++ src/main/java/com/smile/controller/PostController.java
@@ -1,7 +1,16 @@
 package com.smile.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
+
+    @GetMapping()
+    public String test(Model model) {
+        model.addAttribute("test", "Gyunny");
+        return "test";
+    }
+
 }
