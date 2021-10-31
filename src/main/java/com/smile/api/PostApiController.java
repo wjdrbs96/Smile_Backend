23876e9d2dd63a8e1082c8007f5ec73dd795d30e
Gyunny <wjdrbs966@naver.com>
2021-10-31 23:50:06 +0900

    Feat: 메인 페이지 페이징 처리

--- src/main/java/com/smile/api/PostApiController.java
+++ src/main/java/com/smile/api/PostApiController.java
@@ -2,6 +2,7 @@ package com.smile.api;
 
 import com.smile.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static com.smile.entity.Paging.createPaging;
+
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/post")
 @Controller
@@ -20,8 +23,12 @@ public class PostApiController {
     private final PostService postService;
 
     @GetMapping
-    public String main(Model model) {
-        model.addAttribute("post", postService.findAll());
+    public String main(@RequestParam(defaultValue = "1") int page,
+                       @RequestParam(defaultValue = "7") int pageSize,
+                       Model model) {
+        PageRequest paging = PageRequest.of(page - 1, pageSize);
+        model.addAttribute("post", postService.findAllByUserOrderByIdDesc(paging));
+        model.addAttribute("paging", createPaging(page, pageSize, (postService.count() / pageSize) + 1));
         return "main";
     }
 
--- src/main/java/com/smile/repository/PostRepository.java
+++ src/main/java/com/smile/repository/PostRepository.java
@@ -2,12 +2,13 @@ package com.smile.repository;
 
 import com.smile.entity.Post;
 import com.smile.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByUser(User user);
+    List<Post> findAllByUserOrderByIdDesc(User user, Pageable pageable);
 
 }
