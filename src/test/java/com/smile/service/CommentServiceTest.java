89ea4154c851c185abb28a135a2fa2585f677684
Gyunny <wjdrbs966@naver.com>
2021-11-04 21:29:08 +0900

    Test: 댓글이 잘 수정되는지 테스트

--- src/test/java/com/smile/service/CommentServiceTest.java
+++ src/test/java/com/smile/service/CommentServiceTest.java
@@ -3,7 +3,6 @@ package com.smile.service;
 import com.smile.entity.Category;
 import com.smile.entity.Comment;
 import com.smile.entity.Post;
-import com.smile.entity.User;
 import com.smile.error.EntityNotFoundException;
 import com.smile.repository.CommentRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static com.smile.entity.Comment.createComment;
 import static com.smile.entity.User.createUser;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * created by Gyunny 2021/11/04
@@ -49,6 +48,17 @@ class CommentServiceTest {
         assertThatThrownBy(() -> {
             commentRepository.findById(saveComment.getId()).orElseThrow(() -> new EntityNotFoundException("Entity is Not Empty!!"));
         }).isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @DisplayName("댓글이 잘 수정되는지 테스트")
+    @Test
+    void updateCommentTest() {
+        String updateComment = "변경 후 댓글";
+        Comment saveComment = commentRepository.save(createComment(createPost(), "변경 전 댓글"));
+
+        commentService.update(saveComment.getId(), updateComment);
+
+        assertEquals(updateComment, saveComment.getComment());
     }
 
     private Post createPost() {
